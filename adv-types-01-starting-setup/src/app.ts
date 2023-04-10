cd1f3fe3d4bef753effc914430ea2e2855cebad8
rrmangum <rrmangum@gmail.com>
2023-04-09 19:47:55 -0700

    advanced TypeScript features

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -23,6 +23,11 @@ type Numeric = number | boolean;
 // intersection type
 type Universal = Combinable & Numeric;
 
+// function overloads
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
 // type guards
 // typeof for JavaScript types
 function add(a: Combinable, b: Combinable) {
@@ -31,6 +36,22 @@ function add(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+
+const result = add("Ryan", "Nathalie");
+result.split(" ");
+
+// optional chaining operator safely accessses nested objects for checking whether or not the object in the chain is undefined, and if it is, then doesn't throw a runtime error
+const fetchedUserData = {
+  id: "u1",
+  name: "Ryan",
+  job: { title: "CEO", description: "My own company" },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+// nullish coalescing operator if userInput is null or undefined then use fallback value
+const userInput = null;
+const storedData = userInput ?? "DEFAULT";
 
 type UnknownEmployee = Employee | Admin;
 
@@ -69,12 +90,72 @@ type Vehicle = Car | Truck;
 const v1 = new Car();
 const v2 = new Truck();
 
+// instanceof operator
 function useVehicle(vehicle: Vehicle) {
   vehicle.drive();
-  if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
     vehicle.loadCargo(1000);
   }
 }
 
 useVehicle(v1);
 useVehicle(v2);
+
+// discriminated unions good to use with interfaces, because you can't use instanceof
+interface Bird {
+  // literal type
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  // literal type
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  // type safety through a switch statement
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 15 });
+
+// type casting
+// exclamation mark is telling TypeScript the expression in front will never yield null.
+// we as the developer know that element is not null use the exclamation mark syntax otherwise do a type check
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
+
+// type casting in React with as keyword this is to avoid using the JSX syntax
+const userInputElement = document.getElementById(
+  "user-input"
+)! as HTMLInputElement;
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
+
+userInputElement.value = "Hi there!";
+
+// index type
+interface ErrorContainer {
+  // I don't know the property name or the count, I just know that every property that's added the property name is a string and the property value is a string
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email",
+  username: "Must start with a capital character!",
+};
+
+// function overloads
